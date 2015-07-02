bbf3baa802d63bb36a1af1ab050beaa0cf93b282
Douglas F Shearer <dougal.s@gmail.com>
2015-07-02 11:28:49 +0100

    Simplest possible failing case.
    greater_that: 0 validation fails when using a model currency.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+
+  monetize :price_cents, with_model_currency: :currency, numericality: { greater_than: 0 }
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_is_valid_with_positive_price
+    product = Product.new(price_cents: 1299)
+
+    assert product.valid?
+  end
+
+  def test_is_invalid_with_zero_price
+    product = Product.new(price_cents: 0)
+
+    assert product.invalid?
+  end
+
+  def test_is_invalid_with_negative_price
+    product = Product.new(price_cents: -1)
+
+    assert product.invalid?
+  end
+
 end
